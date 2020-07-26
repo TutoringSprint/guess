5cd4099e428eff361ce9b53e157119c3a24022cd
Huy Au <duong.huy.au@gmail.com>
2020-07-26 18:27:21 +1000

    Updates

---
+++ precache-manifest.e6d82d4719425bb2d62935dade8cdfa4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a26182ff92590d2bbe34b74d3f7006dd",
+    "url": "/guess/index.html"
+  },
+  {
+    "revision": "d984c7a246cb63d6612f",
+    "url": "/guess/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4ce265d8695bb7375672",
+    "url": "/guess/static/js/2.353b10ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/guess/static/js/2.353b10ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d984c7a246cb63d6612f",
+    "url": "/guess/static/js/main.13a8cee5.chunk.js"
+  },
+  {
+    "revision": "9a7086615e1f77c23211",
+    "url": "/guess/static/js/runtime-main.629665a4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/guess/static/media/logo.5d5d9eef.svg"
+  }
+]);
